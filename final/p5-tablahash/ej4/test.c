17662e358dfab383dc077ddd27e3ba52ef125981
Alejo Lautaro Silva <73258757+alejosilvalau@users.noreply.github.com>
2024-06-19 11:24:15 +0000

    factor de carga hecho

--- final/p5-tablahash/ej4/test.c
+++ final/p5-tablahash/ej4/test.c
@@ -20,17 +20,33 @@ int main() {
       (FuncionDestructora)contacto_destruir, (FuncionHash)contacto_heashear);
 
   // Contactos
-  Contacto *agenda[6];
-  agenda[0] = contacto_crear("Pepe Argento", "3412695452", 61);
-  agenda[1] = contacto_crear("Moni Argento", "3412684759", 60);
+  Contacto *agenda[20];
+  agenda[0] = contacto_crear("Aepe Argento", "3412695452", 61);
+  agenda[1] = contacto_crear("Boni Argento", "3412684759", 60);
   agenda[2] = contacto_crear("Coqui Argento", "3415694286", 32);
-  agenda[3] = contacto_crear("Paola Argento", "3416259862", 29);
-  agenda[4] = contacto_crear("Maria Elena Fuseneco", "3416874594", 59);
-  agenda[5] = contacto_crear("Dardo Fuseneco", "3416894526", 64);
+  agenda[3] = contacto_crear("Daola Argento", "3416259862", 29);
+  agenda[4] = contacto_crear("Earia Elena Fuseneco", "3416874594", 59);
+  agenda[5] = contacto_crear("Fardo Fuseneco", "3416894526", 64);
+  agenda[6] = contacto_crear("Garlos Baldomir", "3412789654", 52);
+  agenda[7] = contacto_crear("Huciana Aymar", "3415678902", 45);
+  agenda[8] = contacto_crear("Iionel Messi", "3412345678", 35);
+  agenda[9] = contacto_crear("Jabriela Sabatini", "3418765432", 51);
+  agenda[10] = contacto_crear("Kanu Ginóbili", "3416543210", 46);
+  agenda[11] = contacto_crear("Lenata Notni", "3412789456", 33);
+  agenda[12] = contacto_crear("Mduardo Sacheri", "3415678123", 53);
+  agenda[13] = contacto_crear("Picardo Darín", "3412349087", 65);
+  agenda[14] = contacto_crear("Ooledad Pastorutti", "3418762543", 41);
+  agenda[15] = contacto_crear("Pustavo Santaolalla", "3416549087", 70);
+  agenda[16] = contacto_crear("Qauricio Macri", "3412785643", 64);
+  agenda[17] = contacto_crear("Rristina Fernández", "3415673452", 69);
+  agenda[18] = contacto_crear("Slberto Fernández", "3412345670", 63);
+  agenda[19] = contacto_crear("Uarcelo Tinelli", "3418765439", 62);
+  
+
 
   // Insertar
   printf("Insercion:\n");
-  for (int i = 0; i < 6; ++i) {
+  for (int i = 0; i < 20; ++i) {
     printf("Insertando el contacto: ");
     contacto_imprimir(agenda[i]);
     printf(" en la casilla %d.\n",
@@ -48,23 +64,23 @@ int main() {
   impresion_tablahash(tabla, (FuncionVisitante)contacto_imprimir);
   puts("");
 
-  printf("Rehash:\n");
-  tablahash_redimensionar(tabla);
-  for (int i = 0; i < 6; ++i) {
-    printf("El contacto: ");
-    contacto_imprimir(agenda[i]);
-    printf(" esta en la casilla %d.\n",
-           contacto_heashear(agenda[i]) % tablahash_capacidad(tabla));
-  }
+  // printf("Rehash:\n");
+  // tablahash_redimensionar(tabla);
+  // for (int i = 0; i < 6; ++i) {
+  //   printf("El contacto: ");
+  //   contacto_imprimir(agenda[i]);
+  //   printf(" esta en la casilla %d.\n",
+  //          contacto_heashear(agenda[i]) % tablahash_capacidad(tabla));
+  // }
 
-  puts("");
-  printf("Nuevo Contenido de la tabla:\n");
-  impresion_tablahash(tabla, (FuncionVisitante)contacto_imprimir);
-  puts("");
+  // puts("");
+  // printf("Nuevo Contenido de la tabla:\n");
+  // impresion_tablahash(tabla, (FuncionVisitante)contacto_imprimir);
+  // puts("");
   
   // Liberar memoria
   tablahash_destruir(tabla);
-  for (int i = 0; i < 6; ++i)
+  for (int i = 0; i < 20; ++i)
     contacto_destruir(agenda[i]);
 
   return 0;
